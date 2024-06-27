f1731c2a26410a7180a134b49eca074c74c211ce
Kristian-A <kristian.zh.apostolov@gmail.com>
2024-06-27 13:18:20 +0100

    Break test

--- src/spec/app/app.component.spec.ts
+++ src/spec/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('h1')?.textContent).toContain('Hello there! remix-plugin');
+        expect(compiled.querySelector('h1')?.textContent).toContain('Hello there remix-plugin');
     });
 });
