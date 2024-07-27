ef255b8185c44ca973868710c82a0927d51e82b7
Kristian-A <kristian.zh.apostolov@gmail.com>
2024-07-27 17:27:27 +0100

    Detect selected file change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RemixClientService } from '../remix-client/remix-client.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
-export class AppComponent {
-    title = 'remix-plugin';
+export class AppComponent implements OnInit, OnDestroy {
+    title = 'remix-plugin2';
+    files: string[] = [];
+    currentFile: string = 'No file selected';
+    private subscription?: Subscription;
+
+    constructor(private clientService: RemixClientService) { }
+
+    ngOnInit(): void {
+        this.subscription = this.clientService.getCurrentFileObservable().subscribe(filename => {
+            this.currentFile = filename;
+        });
+    }
+
+    loadFiles(): void {
+        this.clientService.listdir().then(files => {
+            this.files = Object.keys(files);
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.subscription?.unsubscribe();
+    }
 }
