de7820a428abcac65b9f86dba485c1395359ccf8
Kristian-A <kristian.zh.apostolov@gmail.com>
2024-08-01 18:03:23 +0100

    Link info panel

--- src/app/info-panel/info-panel.component.ts
+++ src/app/info-panel/info-panel.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-info-panel',
-	standalone: true,
-	imports: [],
 	templateUrl: './info-panel.component.html',
 	styleUrl: './info-panel.component.scss'
 })
--- src/app/info-panel/info-panel.module.ts
+++ src/app/info-panel/info-panel.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InfoPanelComponent } from './info-panel.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [InfoPanelComponent],
     imports: [
         CommonModule
-    ]
+    ],
+    exports: [InfoPanelComponent],
 })
 export class InfoPanelModule { }
