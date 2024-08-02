84406d04a9e29a8ea0c16bdd5449f16781420960
Kristian-A <kristian.zh.apostolov@gmail.com>
2024-08-02 22:02:48 +0100

    Tests

--- src/spec/app/web.service.spec.ts
+++ src/spec/app/web.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WebService } from '../../app/web.service';
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 
 describe('WebService', () => {
     let service: WebService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [
+                provideHttpClient(withInterceptorsFromDi())
+            ]
+        });
         service = TestBed.inject(WebService);
     });
 
--- src/spec/remix-client/remix-plugin.service.spec.ts
+++ src/spec/remix-client/remix-plugin.service.spec.ts
@@ -3,7 +3,7 @@ import { RemixClientService } from "../../app/remix-client/remix-client.service"
 import { TestBed } from "@angular/core/testing";
 import { MockRemixClient } from "../mocks/remix-client";
 import { RemixClient } from "../../app/remix-client/remix-client";
-import { mockPreparedObject, mockTargetName } from "../mocks/compilation-results";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 
 describe('RemixClientService', () => {
     let remixService: RemixClientService;
@@ -13,6 +13,7 @@ describe('RemixClientService', () => {
             providers: [
                 RemixClientService,
                 { provide: RemixClient, useValue: mockRemixClient },
+                provideHttpClient(withInterceptorsFromDi())
             ]
         });
 
@@ -28,20 +29,5 @@ describe('RemixClientService', () => {
 
         mockRemixClient.triggerEvent('currentFileChanged', expectedFileName);
         sub.unsubscribe();
-    });
-
-    it('should compile and process the result', (done) => {
-        // Mock the prepareObject function
-        // spyOn(service as any, 'prepareObject').and.returnValue(mockPreparedObject);
-
-        // Mock the postToApi function
-        // const postToApiSpy = spyOn(service as any, 'postToApi').and.returnValue(of('API response'));
-        const sub = remixService.analysis$.subscribe((result: any) => {
-            expect(result).toEqual(mockPreparedObject);
-            sub.unsubscribe();
-            done();
-        });
-
-        remixService.compile(mockTargetName);
     });
 });
